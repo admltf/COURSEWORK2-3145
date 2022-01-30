941efa2bd0050e45a4390fc9e896352ea3348562
admltf <adamlatif4@hotmail.com>
2022-01-30 21:25:51 +0000

    put/del

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var bodyParser = require("body-parser")
 
 app.use(express.json())
 
@@ -36,11 +37,33 @@ app.post('/collection/:collectionName', (req, res, next) => {
 
 const ObjectID = require('mongodb').ObjectId
 app.get('/collection/:collectionName/:id', (req, res, next) => {
-    req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
+    req.collection.findOne({ _id: new ObjectID((req.params.id).trim()) }, (e, result) => {
         if (e) return next (e)
         res.send(result)
     })
 })
+
+app.put('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.updateOne(
+        {_id: new ObjectID((req.params.id).trim())},
+        {$set: req.body},
+        {safe: true, multi: false},
+        (e, result) => {
+            if (e) return next(e)
+            res.send(result.result === 1 ? {msg: 'success'} : {msg: 'error'})
+        }
+    )
+}) 
+
+app.delete('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.deleteOne(
+        {_id: ObjectID(req.params.id)},
+        (e, result) => {
+            if (e) return next(e)
+            res.send((result.result === 1) ? {msg: 'success'} : {msg: 'error'})
+        })
+})
+
 
 app.listen(3000, function() {
     console.log("Listening on 3000")
